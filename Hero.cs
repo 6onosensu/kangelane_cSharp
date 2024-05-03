24d83c951ee7404e8e1818c7f6286efbfb706fa0
Dasha <darja.portnova@gmail.com>
2024-05-03 13:53:29 +0300

    modified code: hero and superhero classes (deleted comments, )

--- Hero.cs
+++ Hero.cs
@@ -11,14 +11,12 @@ namespace kangelane_cSharp
         private string name;
         private string location;
 
-        // constructor
         public Hero(string name, string location)
         {
             this.name = name;
             this.location = location;
         }
 
-        // get and set methods for name and location
         public string Name
         {
             get { return name; }
@@ -31,10 +29,9 @@ namespace kangelane_cSharp
             set { location = value; }
         }
 
-        // methods Save and toString
-        public virtual int Save(int numOfPeople)
+        public virtual int NumOfSavedPeople(int num)
         {
-            return (int)Math.Round(numOfPeople * 0.95);
+            return (int)Math.Round(num * 0.95);
         }
         public override string ToString()
         {
--- SuperHero.cs
+++ SuperHero.cs
@@ -8,28 +8,21 @@ namespace kangelane_cSharp
 {
     public class SuperHero: Hero
     {
-        private static Random random = new Random();
+        private static Random rand = new Random();
         private double skill;
 
         public double GetSkill() { return skill; }
 
-        public SuperHero(string name, string location) 
-            : base(name, location)
+        public SuperHero(string name, string location)
         {
-            Random random = new Random();
-            // returns a random floating-point number
-            // that is greater than or wqual to 0.0, and less than 1.0
-            skill = random.NextDouble() * 4 + 1; 
+            this.skill = rand.NextDouble() * 4 + 1; 
         }
 
-        //The override modifier is required to extend
-        //or modify the abstract or virtual implementation
-        //of an inherited method, property, indexer, or event.
-        public override int Save(int numOfPeople)
+        public override int NumOfSavedPeople(int num)
         {
-            double savePercentage = 95 + skill;
-            return (int)Math.Round(numOfPeople * (savePercentage / 100));
+            return (int)Math.Round(num * ((95 + skill) / 100));
         }
+
         public override string ToString()
         {
             return $"{base.ToString()}, Skill: {skill}";
